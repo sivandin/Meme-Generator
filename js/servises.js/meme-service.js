d1c906346f2096c1c6e064460379bc97b673f948
sivandin <sivandin@gmail.com>
2024-03-15 17:02:06 +0200

    Add meme-service, add getMeme function

---
+++ js/servises.js/meme-service.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var gImgs = [{id: 3, url: 'img/3.jpg', keywords: ['cute', 'baby']}]
+var gMeme = {
+ selectedImgId: 2,
+ selectedLineIdx: 0,
+ lines: [
+ {
+ txt: 'Write down your text here',
+ size: 20,
+ color: 'red'
+ }
+ ]
+}
+var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
+
+function getMeme() {
+    return gMeme
+}
+
+function setLineTxt() {
+    gMeme.lines[0].txt=''
+}
