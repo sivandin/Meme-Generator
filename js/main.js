8ed280f032ad021d3e987615f10a80648c6cac04
sivandin <sivandin@gmail.com>
2024-03-16 19:11:29 +0200

    Style for mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict'
 
-
-
-
+function toggleMenu() {
+    document.body.classList.toggle('menu-open')
+}
 
 // Cover a fixed-width canvas using an img
 // by changing the canvas height
@@ -12,4 +12,14 @@ function resizeCanvas() {
 
     // Changing the canvas dimension clears the canvas
     gElCanvas.width = elContainer.clientWidth
+}
+
+function closeMenuOnClickOutside(event) {
+    var nav = document.querySelector('.main-nav');
+    var toggleMenuBtn = document.querySelector('.toggle-menu-btn');
+    // Close the menu if it's open and the click is outside of the navigation menu and toggle button
+    if (event.target !== nav && event.target !== toggleMenuBtn && !nav.contains(event.target)) {
+        var body = document.querySelector('body');
+        body.classList.remove('menu-open');
+    }
 }
