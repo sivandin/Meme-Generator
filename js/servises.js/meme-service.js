6bbbef7683ba73d3793297b2b7336b0f4cd75433
sivandin <sivandin@gmail.com>
2024-03-17 11:16:12 +0200

    Add Increase decrease and addline buttons

--- js/servises.js/meme-service.js
+++ js/servises.js/meme-service.js
@@ -1,5 +1,6 @@
 'use strict'
 
+let gNextY = 70
 var gMeme = {
     selectedImgId: 2,
     selectedLineIdx: 0,
@@ -12,6 +13,17 @@ var gMeme = {
             fillColor: 'orange',
             fontSize: 35,
             fontFamily: 'Arial',
+            isSelected: true,
+        },
+        {
+            txt: 'Write down your text here',
+            x: 200,
+            y: 60,
+            storkeColor: 'black',
+            fillColor: 'orange',
+            fontSize: 35,
+            fontFamily: 'Arial',
+            isSelected: false,
         }
     ]
 }
@@ -22,7 +34,8 @@ function getMeme() {
 }
 
 function setLineTxt(val) {
-    gMeme.lines[0].txt = val
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].txt = val
     _saveSelectedImg()
 }
 
@@ -37,24 +50,57 @@ function _saveSelectedImg() {
     saveToStorage('selectedMeme', meme)
 }
 
-function changeStrokeColor(val){
-    gMeme.lines[0].storkeColor=val
+function changeStrokeColor(val) {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].storkeColor = val
     _saveSelectedImg()
 }
 
-function changeFillColor(val){
-    gMeme.lines[0].fillColor=val
+function changeFillColor(val) {
+    const lineIdx = gMeme.selectedLineIdx
+    gMeme.lines[lineIdx].fillColor = val
     _saveSelectedImg()
 }
 
-function changeTxtSize(val){
-    if (val==='increase') gMeme.lines[0].fontSize+=2
-    else (gMeme.lines[0].fontSize-=2)
+function changeTxtSize(val) {
+    const lineIdx = gMeme.selectedLineIdx
+    if (val === 'increase') gMeme.lines[lineIdx].fontSize += 2
+    else (gMeme.lines[lineIdx].fontSize -= 2)
     _saveSelectedImg()
 }
 
 
+function addLine() {
 
+    gMeme.lines.push({
+        txt: 'Write down your text here',
+        x: 200,
+        y: gNextY,
+        strokeColor: 'black', // Corrected typo in property name
+        fillColor: 'orange',
+        fontSize: 35,
+        fontFamily: 'Arial',
+        isSelected: 'true'
+    })
 
+    gNextY += 10 // Increase y coordinate for the next line
+    _saveSelectedImg()
+}
 
+function switchLine() {
+var currLineIdx = gMeme.selectedLineIdx
+var numOFLines = gMeme.lines.length
 
+if (currLineIdx<numOFLines-1) gMeme.selectedLineIdx++
+else gMeme.selectedLineIdx=0
+
+gMeme.lines[currLineIdx].isSelected='true'
+console.log(gMeme)
+_saveSelectedImg()
+}
+
+function cleanSelected () {
+    for (var i=0; i<gMeme.lines.length; i++){
+        gMeme.lines[i].isSelected='false'
+    }
+}
